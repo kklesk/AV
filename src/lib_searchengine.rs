ee9c300f1ce89db5a172ac1b43fe6bd9cba7f152
Ivan <i.spina@live.com>
2021-12-27 23:20:03 +0100

    Add Structs: file and SearchEngine

--- src/lib_searchengine.rs
+++ src/lib_searchengine.rs
@@ -1,57 +1,57 @@
 pub mod lib_searchengine{
 
-    enum SearchEngineExtension{
-        Dummy (bool),
-        Pe_file (bool),
-        Jpeg (bool),
-        Pdf (bool),
-        //TODO add all common filetypes
-    }
+    // enum SearchEngineExtension{
+    //     Dummy (bool),
+    //     Pe_File (bool),
+    //     Jpeg (bool),
+    //     Pdf (bool),
+    //     //TODO add all common filetypes
+    // }
     
-    enum SearchEngineIsInfected{
-        IsInfected(bool),
-    }
+    // enum SearchEngineIsInfected{
+    //     IsInfected(bool),
+    // }
     
-    #[derive(Debug)]
-    struct SearchEngine{
+    // #[derive(Debug)]
+    // struct SearchEngine{
     
-        se_root_dir: String,
-        se_current_dir: String,
-        // se_file_extension: SearchEngineExtension,
-        se_is_infected: bool,
-        //TODO search filter
-        // se_filter: enum SearchEngineFilter,
+    //     se_root_dir: String,
+    //     se_current_dir: String,
+    //     // se_file_extension: SearchEngineExtension,
+    //     se_is_infected: bool,
+    //     //TODO search filter
+    //     // se_filter: enum SearchEngineFilter,
         
-    }
+    // }
     
-    impl SearchEngine{
-        fn new() -> Self{
-            Self {
-                se_root_dir: String::from("C:\\"),
-                //TODO Read User from WIndows API (%USERPROFILE%) and Linux 
-                // for directory scan
-                se_current_dir: String::from("C:\\Users\\%USERPROFILE%"),
-                se_is_infected: false,
-                }
-        }
-        fn debug(&self){
-            println!("{:?}",self);
-        }
-        fn start_search(&self){
-            // let path = Path::new(&self.se_current_dir);
-            let p1=String::from("C:\\Users\\Ivan");
+    // impl SearchEngine{
+    //     fn new() -> Self{
+    //         Self {
+    //             se_root_dir: String::from("C:\\"),
+    //             //TODO Read User from WIndows API (%USERPROFILE%) and Linux 
+    //             // for directory scan
+    //             se_current_dir: String::from("C:\\Users\\%USERPROFILE%"),
+    //             se_is_infected: false,
+    //             }
+    //     }
+    //     fn debug(&self){
+    //         println!("{:?}",self);
+    //     }
+    //     fn start_search(&self){
+    //         // let path = Path::new(&self.se_current_dir);
+    //         let p1=String::from("C:\\Users\\Ivan");
     
-             let path = Path::new(&p1);
+    //          let path = Path::new(&p1);
     
-            match fs::read_dir(path) { 
-                Err(why) => println!("{:?}", why.kind()),
-                Ok(path) =>
-                    for file in path{
-                        println!("{:?}",file.unwrap().path());
-                    }
-        }
+    //         match fs::read_dir(path) { 
+    //             Err(why) => println!("{:?}", why.kind()),
+    //             Ok(path) =>
+    //                 for file in path{
+    //                     println!("{:?}",file.unwrap().path());
+    //                 }
+    //     }
     
-        }
-    }
+    //     }
+    // }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,113 @@
 use lib_peparser::lib_peparser::hello;
 use std::path::Path;
+use std::fs;
 use std::fs::{File, OpenOptions};
 use std::io::*;
-use std::fs;
-mod lib_peparser;
-mod lib_searchengine;
 
+/*calc hash values*/
+// use ring::Digest::{Context, Digest, SHA256};
+
+/*load external libs*/
+mod lib_peparser;
+// mod lib_searchengine;
+
+enum SearchEngineFileExtension{
+    Dummy (bool),
+    PeFile (bool),
+    Jpeg (bool),
+    Pdf (bool),
+    //TODO add all common filetypes
+}
+
+#[derive(Debug)]
+enum SearchEngineFileHashes{
+    // FileHashSha1(),
+    // FileHashSha2(),
+}
+impl SearchEngineFileHashes{
+    fn calculate_sha1() /*-> Result<Digest>*/{
+
+    }
+}
+
+/*SearchEngineFile
+
+
+*/
+#[derive(Debug)]
+struct SearchEngineFile{
+    //TODO add File with multiple file headers
+    FileName: String,
+    FileExtension: String,
+    FileSize: u16,
+    // parse meta data creation time, modified time ....
+    // FileCreationDate, 
+    FileHashes: SearchEngineFileHashes,
+    IsMalicious: bool,
+
+
+
+}
+
+#[derive(Debug)]
+struct SearchEngine{
+
+    se_root_dir: String,
+    se_current_dir: String,
+    // se_file: SearchEngineFile,
+    /*TODO Check OS */
+    // Scantime
+    // se_scantime_begin: ,
+    // se_scantime_end: ,
+    // se_file_extension: SearchEngineExtension,
+    //TODO search filter
+    // se_filter: enum SearchEngineFilter,
+    
+}
+
+impl SearchEngine{
+    fn new() -> Self{
+        Self {
+            se_root_dir: String::from("C:\\"),
+            //TODO Read User from WIndows API (%USERPROFILE%) and Linux 
+            // for directory scan
+            se_current_dir: String::from("C:\\Users\\%USERPROFILE%"),
+            }
+    }
+    fn debug(&self){
+        println!("{:?}",self);
+    }
+    fn start_search(&self){
+        // let path = Path::new(&self.se_current_dir);
+        let search_dir=String::from("C:\\Users\\Ivan");
+
+         let path = Path::new(&search_dir);
+
+        match fs::read_dir(path) { 
+            Err(err) => println!("{:?}", err.kind()),
+            Ok(path) =>
+                for file in path{
+                    println!("{:?}",file.unwrap().path());
+                }
+    }
+    fn scan_file(){
+
+    }
+
+    }
+}
 
 
 fn main() {
 
-    let s1=lib_searchengine::SearchEngine::new();
+    let s1=SearchEngine::new();
     s1.debug();
     s1.start_search();
 
+
+    println!(
+        "User's Name            whoami::realname():    {}",
+        whoami::realname()
+    );
      // println!("Hello, world!");
 }
