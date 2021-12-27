b1fc99074066bcd5e7c6241d65635281524e7893
Ivan <i.spina@live.com>
2021-12-27 23:19:36 +0100

    add structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use lib_peparser::lib_peparser::hello;
+use std::path::Path;
+use std::fs::{File, OpenOptions};
+use std::io::*;
+use std::fs;
+mod lib_peparser;
+mod lib_searchengine;
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let s1=lib_searchengine::SearchEngine::new();
+    s1.debug();
+    s1.start_search();
+
+     // println!("Hello, world!");
 }
