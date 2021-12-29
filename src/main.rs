ae6e1ea5122ee41aeac33b2857e6cbd65bfad6fc
Ivan <i.spina@live.com>
2021-12-29 23:33:16 +0100

    add hash function; compile error at #49

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use lib_peparser::lib_peparser::hello;
 use std::path::Path;
 use std::fs;
 use std::fs::{File, OpenOptions};
-use std::io::*;
+use std::io;
 
 /*calc hash values*/
-use ring::digest;
+use ring::digest::{Context, Digest, SHA256, SHA512};
+use std::io::{BufReader, Read, Write};
+use blake3::*;
+// use data_encoding::HEXUPPER;
 
 /*load external libs*/
 mod lib_peparser;
@@ -21,15 +24,47 @@ enum SearchEngineFileExtension{
 
 #[derive(Debug)]
 enum SearchEngineFileHashes{
-    // FileHashSha1(),
-    // FileHashSha2(),
+    Sha1(Digest),
+    Sha2(Digest),
 }
 impl SearchEngineFileHashes{
     fn calculate_sha1() /*-> Result<digest> */{
 
-    }
-    fn calculate_sha2() /*-> Result<digest> */{
+    } 
+    fn calculate_sha2() -> Digest{
+        //TODO add dyn path variable E0277
+        // let mut file_path = Path::new(path);
+        //for debbuging, write static string 
+        let file_path = Path::new("C:\\Users\\Ivan\\development\\AV\\abc.bin");
+        let mut context = Context::new(&SHA256);
+        let mut buffer = [0; 4096];
 
+        let mut file = match File::open(&file_path){
+            Err(err) => panic!("could not open file {} for generate sha256 sum", err),
+            Ok(file) => file,
+        };
+        let buffer_reader = BufReader::new(file);
+
+        loop { 
+            let count = buffer_reader.read(&mut buffer)?;
+            if count == 0 {
+                break;
+            }
+            context.update(&buffer[..count]);
+        }
+        let hash = context.finish();
+        println!("{:?}", hash);
+        hash
+        // Ok(hash)
+        // blake3 library test
+        // let mut hasher = blake3::Hasher::new();
+        // let mut calc_sha2 = [0;1000];
+        // let mut output_reader = hasher.finalize_xof();
+        // output_reader.fill(&mut calc_sha2);
+        // println!("{:?}",calc_sha2);
+        // // let mut output_reader = blake3::Hash(b"hello"); 
+        // // Hasher::hasher.finalzie_xof();
+    
     }
     fn calculate_ssdeep() /*-> Result<digest> */{
 
@@ -50,6 +85,9 @@ struct SearchEngineFile{
     // FileCreationDate, 
     FileHashes: SearchEngineFileHashes,
     IsMalicious: bool,
+}
+
+impl SearchEngineFile{
 
 }
 
@@ -104,14 +142,37 @@ impl SearchEngine{
 
 fn main() {
 
-    let s1=SearchEngine::new();
-    s1.debug();
-    s1.start_search();
 
 
-    println!(
-        "User's Name            whoami::realname():    {}",
-        whoami::realname()
-    );
+   let e = SearchEngineFileHashes::calculate_sha2();
+
+    // let s1=SearchEngine::new();
+    // s1.debug();
+    // s1.start_search();
+
+
+
+    // let hash1 = blake3::hash(b"foobarbaz");
+    // println!("{:?}",hash1);
+
+
+    // let fp = Path::new("C:\\Users\\Ivan\\development\\AV\\abc.bin");
+    // let f = File::open(fp);
+    // let mut buf_reader = BufReader::new(f);
+
+
+
+    // let mut hasher = blake3::Hasher::new();
+
+    // let mut calc_sha2 = [0;1000];
+    // let mut output_reader = hasher.finalize_xof();
+    // output_reader.fill(&mut calc_sha2);
+    // println!("{:?}",calc_sha2);
+
+    // println!(
+    //     "User's Name            whoami::realname():    {}",
+    //     whoami::realname()
+    // );
      // println!("Hello, world!");
+
 }
